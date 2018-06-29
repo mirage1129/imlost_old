638bfb688d057367272fe57c53e2f5c1bf6d6278
mirage1129 <meraj.a.qazi@gmail.com>
2018-06-29 17:51:30 +0800

    Did the first get request posting a header to the homepage

---
+++ views/home.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+class home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Yolo</h1>
+      </div>
+    );
+  }
+}
+
+module.exports = home;
