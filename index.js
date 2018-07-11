a0c14a6352519f9882e68f79415d56d02eafaa50
mirage1129 <meraj.a.qazi@gmail.com>
2018-07-11 11:10:37 +0800

    Heroku stuff added

--- index.js
+++ index.js
@@ -24,4 +24,8 @@ app.get('/', (request, response) => {
   response.render('Home');
 });
 
-app.listen(3000, () => {console.log('Server started')});
+
+const PORT = process.env.PORT || 3000;
+
+const server = app.listen(PORT, () => console.log('~~~ Tuning in to the waves of port '+PORT+' ~~~'));
+// app.listen(3000, () => {console.log('Server started')});
